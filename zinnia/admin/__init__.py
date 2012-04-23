7186b71494ffaac71bfed79613246fe315db0c81
Emilian Felecan <emil@springmerchant>
2012-04-23 10:56:51 +0300

    Adding the Redirect model to the control panel.

--- zinnia/admin/__init__.py
+++ zinnia/admin/__init__.py
@@ -1,11 +1,11 @@
 """Admin of Zinnia"""
 from django.contrib import admin
 
-from zinnia.models import Entry
-from zinnia.models import Category
+from zinnia.models import Entry, Category, Redirect
 from zinnia.admin.entry import EntryAdmin
 from zinnia.admin.category import CategoryAdmin
 
 
 admin.site.register(Entry, EntryAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Redirect)
