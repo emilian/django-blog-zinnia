71ba0b9359af105d2952b039563bd9b041fe6827
Emilian Felecan <emil@springmerchant>
2012-05-02 15:32:46 +0300

    Make sure we take into account subdomains pecific templates for the list of categories.

--- zinnia/urls/categories.py
+++ zinnia/urls/categories.py
@@ -3,8 +3,9 @@ from django.conf.urls.defaults import url
 from django.conf.urls.defaults import patterns
 
 from zinnia.models import Category
+from zinnia.views.decorators import template_name_current_site
 
-category_conf = {'queryset': Category.published.on_site()}
+category_conf = {'queryset': Category.published.on_site(), 'template_name': template_name_current_site('category_list.html') }
 
 urlpatterns = patterns('django.views.generic.list_detail',
                        url(r'^$', 'object_list',
